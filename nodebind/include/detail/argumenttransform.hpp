0e18254adc028711c3d0205d341d48065b0e4f39
Albert Wang <albertywang@gmail.com>
2012-09-30 23:57:15 -0500

    Return storage removing a pointer and a reference to make it instantiable

--- nodebind/include/detail/argumenttransform.hpp
+++ nodebind/include/detail/argumenttransform.hpp
@@ -46,6 +46,16 @@ namespace nodebind
 			};
 		};
 
+		struct IndirectOnce
+		{
+			template<typename T>
+			struct apply
+				: boost::remove_cv<
+					typename boost::remove_reference< typename boost::remove_pointer<T>::type >::type
+				>
+			{};
+		};
+
 		template<typename ReturnType>
 		struct GetReturnStorage 
 			: boost::mpl::if_< 
@@ -60,8 +70,15 @@ namespace nodebind
 		struct ReturnTransformation
 		{
 			typedef typename ArgumentPolicies<Arguments, PolicyList>::type ArgumentPolicies;
-			typedef typename boost::mpl::copy_if<ArgumentPolicies, HasOutPolicy>::type OutParameters;
-			typedef typename boost::mpl::transform<OutParameters, TakeArgument>::type OutArguments;
+			
+			// map( map( copy_if(argumentpolifies, hasoutpolicy), takeargument) indirectonce)
+			typedef typename boost::mpl::transform<
+				typename boost::mpl::transform< 
+					typename boost::mpl::copy_if<ArgumentPolicies, HasOutPolicy>::type, 
+					TakeArgument
+				>::type,
+			IndirectOnce>::type OutArguments;
+
 			typedef typename GetReturnStorage<ReturnType>::type ReturnStorage;
 
 			//This implies that tuple return values cannot use output parameters.
@@ -94,14 +111,16 @@ namespace nodebind
 		template<typename ReturnType, typename Arguments, typename PolicyList>
 		struct SignatureTransformation
 		{
-			//This is a mpl::vector of the return values.
+			//This is a mpl::vector of the return values. Values are indirected once - less one 
+			//pointer, less one reference.
 			typedef typename ReturnTransformation<ReturnType, Arguments, PolicyList>::type ReturnTypes;
 			typedef typename ArgumentTransformation<Arguments, PolicyList> TransformedArguments;
 
 			//This is the signature that is exposed to node.
 			typedef typename TransformedArguments::type ArgumentTypes;
 
-			//This is the storage 
+			//This is the storage for the C++ method. Same as the actual signature, except all 
+			//References are turned into pointers, 
 
 			typedef typename TransformedArguments::IndexMappings ArgumentIndexMappings; 
 		};
